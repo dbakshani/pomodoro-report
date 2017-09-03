e58e253383f150d1cee2d21fd75c25a4eea40160
Dave Bakshani <dave@bakshani.net>
2017-09-03 12:19:34 -0700

    Getting filename as command line parameter and formatting output.

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"regexp"
 	"sort"
+	"strconv"
+	"time"
 )
 
 func main() {
-	file, err := os.Open("pomodoros.txt")
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("The only parameter needed is the name of the pomodoros file.")
+		os.Exit(1)
+	}
+	file, err := os.Open(args[1])
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error: ", err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
@@ -24,9 +31,9 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		parts := r.FindStringSubmatch(line)
-		fmt.Printf("parts: %q\n", parts)
+		//fmt.Printf("parts: %q\n", parts)
 		if len(parts) != 0 {
 			if val, ok := pomodoros[parts[1]]; ok {
 				pomodoros[parts[1]] = val + 1
@@ -37,13 +44,21 @@ func main() {
 		}
 
 	}
-	sort.Strings(dates)
-	//for date, count := range pomodoros {
-	for _, date := range dates {
-		fmt.Printf("%v: %v\n", date, pomodoros[date])
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	sort.Strings(dates)
+	for _, date := range dates {
+		y, _ := strconv.Atoi(date[:4])
+		m, _ := strconv.Atoi(date[4:6])
+		d, _ := strconv.Atoi(date[6:8])
+		l, _ := time.LoadLocation("UTC")
+		//fmt.Println("parts: ", y, m, d)
+		dt := time.Date(y, time.Month(m), d, 0, 0, 0, 0, l)
+		layout := "Mon 2006-01-02"
+
+		fmt.Printf("%v: %v\n", dt.Format(layout), pomodoros[date])
 	}
 }
