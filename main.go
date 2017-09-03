5ee579b11bb45101a69afeb263bf86e5d75007ac
Dave Bakshani <dave@bakshani.net>
2017-09-03 14:00:43 -0700

    Adding comments.

--- main.go
+++ main.go
@@ -23,17 +23,22 @@ func main() {
 	}
 	defer file.Close()
 
+	// mapping of date to pomodoro count
 	pomodoros := make(map[string]int)
+
+	// array of dates for sorting results
 	var dates []string
 
 	var r = regexp.MustCompile(`^(\d{8})@.*(p)$`)
 
+	// process file line-by-line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		//fmt.Println(line)
 		parts := r.FindStringSubmatch(line)
 		//fmt.Printf("parts: %q\n", parts)
+		// only process lines that match the regex
 		if len(parts) != 0 {
 			if val, ok := pomodoros[parts[1]]; ok {
 				pomodoros[parts[1]] = val + 1
@@ -50,15 +55,15 @@ func main() {
 	}
 
 	sort.Strings(dates)
+
 	for _, date := range dates {
 		y, _ := strconv.Atoi(date[:4])
 		m, _ := strconv.Atoi(date[4:6])
 		d, _ := strconv.Atoi(date[6:8])
 		l, _ := time.LoadLocation("UTC")
-		//fmt.Println("parts: ", y, m, d)
 		dt := time.Date(y, time.Month(m), d, 0, 0, 0, 0, l)
 		layout := "Mon 2006-01-02"
 
-		fmt.Printf("%v: %v\n", dt.Format(layout), pomodoros[date])
+		fmt.Printf("%v: %v\t%v\n", dt.Format(layout), pomodoros[date], nil)
 	}
 }
