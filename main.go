ffd29676fea5acbfda291da6cca425cee92b5194
Dave Bakshani <dave@bakshani.net>
2017-09-03 15:05:36 -0700

    Updating to print all dates, even if there were no pomodoros on that date.

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	defer file.Close()
 
 	// mapping of date to pomodoro count
-	pomodoros := make(map[string]int)
+	pomodoros := make(map[time.Time]int)
 
 	// array of dates for sorting results
 	var dates []string
@@ -40,11 +40,12 @@ func main() {
 		//fmt.Printf("parts: %q\n", parts)
 		// only process lines that match the regex
 		if len(parts) != 0 {
-			if val, ok := pomodoros[parts[1]]; ok {
-				pomodoros[parts[1]] = val + 1
+			t := convertToTime(parts[1])
+			if val, ok := pomodoros[t]; ok {
+				pomodoros[t] = val + 1
 			} else {
-				pomodoros[parts[1]] = 1
-				dates = append(dates, parts[1])
+				pomodoros[t] = 1
+				dates = append(dates, t.Format(time.RFC3339))
 			}
 		}
 
@@ -54,16 +55,44 @@ func main() {
 		os.Exit(1)
 	}
 
+	var processingDate time.Time
+
 	sort.Strings(dates)
 
-	for _, date := range dates {
-		y, _ := strconv.Atoi(date[:4])
-		m, _ := strconv.Atoi(date[4:6])
-		d, _ := strconv.Atoi(date[6:8])
-		l, _ := time.LoadLocation("UTC")
-		dt := time.Date(y, time.Month(m), d, 0, 0, 0, 0, l)
-		layout := "Mon 2006-01-02"
-
-		fmt.Printf("%v: %v\t%v\n", dt.Format(layout), pomodoros[date], nil)
+	if len(dates) > 0 {
+		// find the earliest date from the provided file
+		//processingDate = convertToTime(dates[0])
+		processingDate.UnmarshalText([]byte(dates[0]))
+	} else {
+		fmt.Println("No pomodoro data found.")
+		os.Exit(2)
 	}
+
+	//	for _, date := range dates {
+	//		//y, _ := strconv.Atoi(date[:4])
+	//		//m, _ := strconv.Atoi(date[4:6])
+	//		//d, _ := strconv.Atoi(date[6:8])
+	//		//l, _ := time.LoadLocation("UTC")
+	//		//dt := time.Date(y, time.Month(m), d, 0, 0, 0, 0, l)
+	//		dt := convertToTime(date)
+	//		layout := "Mon 2006-01-02"
+	//
+	//		fmt.Printf("%v: %v\t%v\n", dt.Format(layout), pomodoros[date], nil)
+	//	}
+
+	for processingDate.Before(time.Now().UTC()) {
+		//fmt.Printf("%v: %v\n", processingDate, pomodoroForDate(pomodoros, processingDate))
+		fmt.Printf("%v: %v\n", processingDate, pomodoros[processingDate])
+
+		processingDate = processingDate.Add(time.Hour * 24)
+	}
+}
+
+// convertToTime return a Time object from the provided date string in yyyymmdd format.
+func convertToTime(date string) time.Time {
+	y, _ := strconv.Atoi(date[:4])
+	m, _ := strconv.Atoi(date[4:6])
+	d, _ := strconv.Atoi(date[6:8])
+	l, _ := time.LoadLocation("UTC")
+	return time.Date(y, time.Month(m), d, 0, 0, 0, 0, l)
 }
